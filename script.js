92a7cfa8bc90d6baf9fdba70feeebac95bb65a6c
Pallavi-A-12 <pallavibhavani1@gmail.com>
2026-08-19 12:02:03 +0530

    updated UX for Switching Login/signup

---
+++ script.js
@@ -0,0 +1,28 @@
+
+//  Login / SignUp Switching
+
+const loginTab = document.getElementById("loginTab");
+const signupTab = document.getElementById("signupTab");
+
+const loginForm = document.getElementById("loginForm");
+const signupForm = document.getElementById("signupForm");
+
+loginTab.addEventListener("click", () => {
+
+    loginForm.classList.remove("hidden");
+    signupForm.classList.add("hidden");
+
+    loginTab.classList.add("active");
+    signupTab.classList.remove("active");
+
+});
+
+signupTab.addEventListener("click", () => {
+
+    signupForm.classList.remove("hidden");
+    loginForm.classList.add("hidden");
+
+    signupTab.classList.add("active");
+    loginTab.classList.remove("active");
+
+});
